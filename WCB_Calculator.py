45a52797f25df4a269236f19ca2e58609a2af403
DanCressUK <78384814+DanCressUK@users.noreply.github.com>
2024-04-04 08:59:54 +0100

    Add files via upload

--- WCB_Calculator.py
+++ WCB_Calculator.py
@@ -1,41 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  3 16:06:36 2024
+##############################################################
+# Simple calculator of whole circle bearings,                #
+# based on two points A and B,                               #
+# each with a respective Easting (m) and Northing (m) value  #
+##############################################################
 
-@author: Dan
-"""
-#Known coordinates, known location
-
-#Calc WCB/AB, WCB/BA. A/B is from Point A to B
 #WCB = Angle from North clockwise toward Line AB
 
 import math
 
 class PointA:
-    E = 258.971
-    N = 490.856
+    E = 258.971 #Input Point A Easting here
+    N = 490.856 #Input Point A Northing here
     
 class PointB:
-    E = 276.722
-    N = 523.555
+    E = 276.722 #Input Point B Easting here
+    N = 523.555 #Input Point B Easting here
 
-#Calculate Deltas
+#Calculate Deltas (Point B - Point A)
 
-DEab = PointB.E - PointA.E
-DNab = PointB.N - PointA.N
+DE = PointB.E - PointA.E
+DN = PointB.N - PointA.N
 
 #Calculate Quadrant Bearing QAB
-QBab = math.atan(abs(DEab)/abs(DNab))
+QBab = math.atan(abs(DE)/abs(DN))
 
-#print(degrees, minutes, seconds)
-
-if DEab > 0 and DNab > 0:
+if DE > 0 and DN > 0:
     WCBab = QBab
-elif DEab > 0 and DNab < 0:
+elif DE > 0 and DN < 0:
     WCBab = 3.14159 - QBab
-elif DEab < 0 and DNab < 0:
+elif DE < 0 and DN < 0:
     WCBab = 3.14159 + QBab
-elif DEab < 0 and DNab > 0:
+elif DE < 0 and DN > 0:
     WCBab = 6.28319 - QBab
 
 if WCBab < 3.14159 :
@@ -51,5 +46,8 @@ BAdegrees = round(WCBba * 180/(math.pi), 3)
 BAminutes = round((BAdegrees % 1) * 60, 3)
 BAseconds = round((BAminutes % 1) * 60, 3)
 
+distance = round(((PointB.E - PointA.E)**2 + (PointB.N - PointA.N)**2)**0.5, 3) #** to the power of
+
 print("WCB AB is: ", ABdegrees, "°", ABminutes, "'", ABseconds, "\"")
 print("WCB BA is: ", BAdegrees, "°", BAminutes, "'",  BAseconds, "\"")
+print("The distance is: ", distance, "m")
